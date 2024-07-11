28b7e3b663e51907264ab8d014e736f8d72627da
tofu345 <shammahayotunde1@gmail.com>
2024-07-11 15:14:44 +0100

    day_12 part one :<

---
+++ 2021/day_12_hs/app/Main.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import qualified Data.Map as Map
+import Data.List.Split
+import Data.List
+import Data.Char
+import Data.Maybe
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let caves = parse contents
+    -- putStrLn contents
+    -- print caves
+
+    partOne caves
+    -- partTwo caves
+
+type Caves = Map.Map String [String]
+
+parse :: String -> Caves
+parse contents =
+    foldl
+        (\acc v ->
+            let [x, y] = splitOn "-" v
+            in insertNotStart y x (insertNotStart x y acc))
+        Map.empty
+        $ lines contents
+
+    where insertNotStart k v map =
+            if v == "start"
+                then map
+                else Map.insertWith (++) k [v] map
+
+type Path = [String]
+
+partOne :: Caves -> IO ()
+partOne caves = do
+    let res = recurse ["start"] [] []
+    putStr "Part One: "
+    print (length res)
+    return ()
+
+    where recurse :: [String] -> Path -> [Path] -> [Path]
+          recurse [] _ acc = acc
+          recurse ("end":rest) path acc =
+              recurse rest path $ ("end" : path) : acc
+          recurse (curr:rest) path acc =
+                if all isLower curr && curr `elem` path
+                    then recurse rest path acc
+                    else
+                        let acc' = recurse rest path acc
+                            connected = fromJust $ Map.lookup curr caves
+                        in recurse connected (curr : path) acc'
+
+-- im lost
+-- partTwo :: 
