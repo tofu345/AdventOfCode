fe090c3cc245ffc8d675468cecef6410d8181d94
tofu345 <shammahayotunde1@gmail.com>
2024-08-20 20:47:11 +0100

    2015/22 v4 hs done :>

--- 2015/day_22_hs/app/Main.hs
+++ 2015/day_22_hs/app/Main.hs
@@ -2,36 +2,25 @@
 
 module Main where
 
-import System.IO
-import System.Exit
-import Data.Foldable
-import Data.Maybe
-import Data.Ord
-import Control.Applicative
 import Control.Monad
-import Control.Monad.Writer
-import Control.Concurrent
 
 initialState = State
-    -- { playerHp = 50
-    -- , playerMana = 500
-    -- , bossHp = 58
-    -- , bossDmg = 9
-    { playerHp = 10
-    , playerMana = 250
-    , bossHp = 14
-    , bossDmg = 8
-    , spellsCast = []
+    { playerHp = 50
+    , playerMana = 500
+    , bossHp = 58
+    , bossDmg = 9
+    , manaSpent = 0
     , effects = []
     }
 
 main = do
-    partOne initialState
+    partOne
+    partTwo
 
 data State = State
     { playerHp :: Int
     , playerMana :: Int
-    , spellsCast :: [Spell]
+    , manaSpent :: Int
     , effects :: [Effect]
     , bossHp :: Int
     , bossDmg :: Int
@@ -49,24 +38,23 @@ spells = enumFrom MagicMissile
 
 data Effect = Effect
     { name :: String
-    , roundsLeft :: Int
+    , rounds :: Int
     , effect :: State -> State
     }
 
 instance Show Effect where
-    show Effect {name, roundsLeft} = name ++ "(" ++ show roundsLeft ++ ")"
+    show Effect {name, rounds} = name ++ "(" ++ show rounds ++ ")"
 
 instance Eq Effect where
     Effect {name = n1} == Effect {name = n2} = n1 == n2
 
 instance Eq State where
-    State {spellsCast = m1, effects = e1}
-        == State {spellsCast = m2, effects = e2} =
-        sum (map cost m1) == sum (map cost m2) && e1 == e2
+    State {manaSpent = m1, effects = e1}
+        == State {manaSpent = m2, effects = e2} =
+        m1 == m2 && e1 == e2
 
 instance Ord State where
-    State {spellsCast = m1} <= State {spellsCast = m2} =
-        sum (map cost m1) <= sum (map cost m2)
+    State {manaSpent = m1} <= State {manaSpent = m2} = m1 <= m2
 
 getEffect Shield = Just shield
 getEffect Poison = Just poison
@@ -93,39 +81,56 @@ check f st@State{bossHp, playerHp, playerMana}
     | playerHp <= 0 || playerMana <= 0 = Nothing
     | otherwise = f st
 
-applyEffects :: State -> State
-applyEffects st@State {effects} =
+cast :: Spell -> State -> Maybe State
+cast MagicMissile st@State{bossHp} = Just st {bossHp = bossHp - 4}
+cast Drain st@State{bossHp, playerHp} = Just st
+    {bossHp = bossHp - 2, playerHp = playerHp + 2}
+cast spell st@State{effects} = do
+    effect <- getEffect spell
+    guard (effect `notElem` effects)
+    Just st {effects = effect : effects}
+
+easy :: State -> Maybe State
+easy st@State {effects} =
     let st' = foldl (flip effect) st effects
-        effects' = map (\e@Effect{roundsLeft} -> e {roundsLeft = roundsLeft - 1}) effects
-    in st' { effects = filter (\Effect {roundsLeft} -> roundsLeft >= 1) effects' }
+        effects' =
+            map (\e@Effect{rounds} -> e {rounds = rounds - 1})
+            effects
+    in Just st' { effects =
+            filter (\Effect {rounds} -> rounds >= 1) effects' }
 
-player :: Spell -> State -> Maybe State
-player sp st = do
-    check (Just . applyEffects) st
-        >>= check (\s@State {playerMana, spellsCast} -> cast sp
-            $ s { playerMana = playerMana - cost sp
-                , spellsCast = sp : spellsCast })
-        >>= check boss
-    where
-    cast :: Spell -> State -> Maybe State
-    cast MagicMissile st@State{bossHp} = Just st {bossHp = bossHp - 4}
-    cast Drain st@State{bossHp, playerHp} = Just st
-        {bossHp = bossHp - 2, playerHp = playerHp + 2}
-    cast spell st@State{effects} = do
-        effect <- getEffect sp
-        guard (effect `notElem` effects)
-        Just st {effects = fromJust (getEffect spell) : effects}
+hard :: State -> Maybe State
+hard st@State {effects, playerHp} =
+    let st' = foldl
+            (flip effect)
+            st {playerHp = playerHp - 1}
+            effects
+        effects' =
+            map (\e@Effect{rounds} -> e {rounds = rounds - 1})
+            effects
+    in Just st' { effects =
+            filter (\Effect {rounds} -> rounds >= 1) effects' }
 
-boss :: State -> Maybe State
-boss st = do
-    let hasShield = shield `elem` effects st
-        bossDmg' = if hasShield then max 1 (bossDmg st - 7) else bossDmg st
-    check (Just . applyEffects) st
-        >>= check (\s@State{playerHp} -> Just s {playerHp = playerHp - bossDmg'})
+player :: (State -> Maybe State) -> Spell -> State -> Maybe State
+player p sp st = do
+    check p st
+        >>= check (\s@State {playerMana, manaSpent} -> cast sp
+                $ s { playerMana = playerMana - cost sp
+                    , manaSpent = manaSpent + cost sp })
+        >>= check (boss p)
+
+boss :: (State -> Maybe State) -> State -> Maybe State
+boss p st = do
+    let bossDmg' = if shield `elem` effects st
+                       then max 1 (bossDmg st - 7)
+                       else bossDmg st
+    check p st
+        >>= check (\s@State{playerHp} ->
+                Just s {playerHp = playerHp - bossDmg'})
         >>= check (\s -> foldl
-                (\acc spell -> minMaybe acc (player spell s))
-                Nothing
-                spells)
+                (\acc spell -> minMaybe acc (player p spell s))
+                    Nothing
+                    spells)
 
 minMaybe :: Maybe State -> Maybe State -> Maybe State
 minMaybe (Just st1) (Just st2) = Just $ min st1 st2
@@ -133,20 +138,21 @@ minMaybe (Just st1) Nothing = Just st1
 minMaybe Nothing (Just st2) = Just st2
 minMaybe _ _ = Nothing
 
-partOne :: State -> IO ()
-partOne s = do
+partOne = do
     putStr "Part One: "
-    putStrLn ""
-    let res = foldl
-            (\acc spell -> minMaybe acc $ player spell s)
-            Nothing spells
-    -- let res = player Recharge s
+    -- let res = foldl
+    --         (\acc spell -> minMaybe acc $ player spell s)
+    --         Nothing spells
+    let res = player easy Poison initialState
     case res of
         Just st -> do
-            print $ reverse (spellsCast st)
-            putStr "Mana Spent = "
-            print $ sum $ map cost $ spellsCast st
+            print $ manaSpent st
         Nothing -> putStrLn "failed"
-    -- return ()
 
--- partTwo ::
+partTwo = do
+    putStr "Part Two: "
+    let res = player hard Poison initialState
+    case res of
+        Just st -> do
+            print $ manaSpent st
+        Nothing -> putStrLn "failed"
