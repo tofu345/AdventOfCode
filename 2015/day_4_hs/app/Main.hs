5e9f5197104f16c49b0fa5f5efd78e1387bfb7bf
tofu345 <shammahayotunde1@gmail.com>
2024-08-31 01:00:43 +0100

    use until day_4_hs

--- 2015/day_4_hs/app/Main.hs
+++ 2015/day_4_hs/app/Main.hs
@@ -3,21 +3,11 @@ module Main where
 import Data.Hash.MD5
 import Data.String.Utils
 
-main :: IO ()
-main = do
-    putStr "Part One: " >> print (partOne 0)
-    putStr "Part Two: " >> print (partTwo 0)
-
 salt = "bgvyzdsv"
 
-partOne num = do
-    if startswith "00000" 
-        $ md5s (Str $ salt ++ show num)
-        then num
-        else partOne (num + 1)
+main = do
+    putStr "Part One: " 
+    print $ until (\n -> startswith "00000" . md5s $ Str (salt ++ show n)) (+1) 0    
 
-partTwo num = do
-    if startswith "000000" 
-        $ md5s (Str $ salt ++ show num)
-        then num
-        else partTwo (num + 1)
+    putStr "Part Two: " 
+    print $ until (\n -> startswith "000000" . md5s $ Str (salt ++ show n)) (+1) 0
