7fdff4b2cc2482e1357623f47d9b60c16a3ffe63
tofu345 <shammahayotunde1@gmail.com>
2025-01-03 16:36:30 +0000

    better

--- 2020/day_1_hs/app/Main.hs
+++ 2020/day_1_hs/app/Main.hs
@@ -6,21 +6,17 @@ main :: IO ()
 main = do
     nums <- map read . lines <$> readFile "input.txt" :: IO [Int]
 
-    let p1 = do
+    putStr "Part One "
+    print $ head $ do
             a <- nums
             b <- nums
             guard (a + b == 2020)
             return (a * b)
 
-    let p2 = do
+    putStr "Part Two "
+    print $ head $ do
             a <- nums
             b <- nums
             c <- nums
             guard (a + b + c == 2020)
             return (a * b * c)
-
-    putStr "Part One "
-    print (head p1)
-
-    putStr "Part Two "
-    print (head p2)
